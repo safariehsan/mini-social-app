9ea01eecd7dade69f4084c321e703914f0d7395c
Ehsan Safari <ehsansafari@hotmail.com>
2022-12-14 17:11:09 +0330

    edit App.tsx file & add env file

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,25 @@
 import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import NotFound from "./pages/NotFound";
+import Navbar from "./components/Navbar";
+import NewPost from "./pages/NewPost";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/new-post" element={<NewPost />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
